2ad82205961b59cb379f3d9f9e34f400cbd7bbed
Thắng Lê Trần <letranthang0606@gmail.com>
2023-06-08 23:39:41 +0700

    first commit

--- core/configs/defaults.py
+++ core/configs/defaults.py
@@ -33,7 +33,7 @@ _C.INPUT.HORIZONTAL_FLIP_PROB_TRAIN = 0.0
 
 _C.DATASETS = CN()
 # List of the dataset names for training, as present in paths_catalog.py
-_C.DATASETS.SOURCE_TRAIN = "gta5_train"
+_C.DATASETS.SOURCE_TRAIN = "gta5_trainafasfd"
 _C.DATASETS.TARGET_TRAIN = ""
 # List of the dataset names for validation, as present in paths_catalog.py
 _C.DATASETS.VALIDATION = ""
